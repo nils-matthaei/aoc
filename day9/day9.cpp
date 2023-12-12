bd7d093888d53dc85fda48feb1250fecbe215817
nils-matthaei <n.matthaei@proton.me>
2023-12-12 08:00:13 +0100

    started day 9

---
+++ day9/day9.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long get_next(vector<long> sequence){
+  
+}
+
+int main(void){
+  fstream infile("sample");  
+  string buf;
+
+  while (getline(infile,buf)){
+    stringstream strem(buf);
+  }
+  
+  return 0;
+}
